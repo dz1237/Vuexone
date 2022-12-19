34127ac59b07b326210102631cd03676836b21d2
dz1237 <2366987785@qq.com>
2022-12-19 20:48:26 +0800

    vuex add one

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
     <h3>App组件</h3>
     <hr>
     <p>Vuex中的state.count的值是:{{ this.$store.state.count }}</p>
+    <p>Vuex中的state.addCount计算属性的值是:{{ this.$store.getters.addCount }}</p>
     <button @click="addCount">+</button>
+    <button @click="subCount">-</button>
+    <button @click="commitCount">同步中直接调用commit方法加3</button>
   </div>
 </template>
 
@@ -14,7 +17,13 @@ export default {
   name: 'App',
   methods:{
     addCount(){
-      
+      this.$store.dispatch("add",2)
+    },
+    subCount(){
+      this.$store.dispatch("sub",2)
+    },
+    commitCount(){
+      this.$store.commit("add",3)
     }
   }
 }
